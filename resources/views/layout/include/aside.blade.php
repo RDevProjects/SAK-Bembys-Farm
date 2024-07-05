d6dc406696f3d6f6a92a672d8aa567d19473c6ad
Ramandhika <ramandhika.ilham@gmail.com>
2024-07-06 03:35:38 +0700

    Refactor aside.blade.php to update icons and text for Entry Unit and Entry Jurnal

--- resources/views/layout/include/aside.blade.php
+++ resources/views/layout/include/aside.blade.php
@@ -42,14 +42,14 @@
                     <li class="sidebar-item">
                         <a class="sidebar-link gap-3 py-2 px-3  rounded-md w-full flex items-center hover:text-blue-600 hover:bg-blue-500"
                             href="{{ route('entry-jurnal.showNamaUnit') }}">
-                            <i class="ti ti-article  text-xl"></i> <span>Entry Unit</span>
+                            <i class="ti ti-building-community  text-xl"></i> <span>Entry Unit</span>
                         </a>
                     </li>
 
                     <li class="sidebar-item">
                         <a class="sidebar-link gap-3 py-2 px-3  rounded-md w-full flex items-center hover:text-blue-600 hover:bg-blue-500"
                             href="{{ route('entry-jurnal') }}">
-                            <i class="ti ti-article  text-xl"></i> <span>Entry Jurnal</span>
+                            <i class="ti ti-notes  text-xl"></i> <span>Entry Jurnal</span>
                         </a>
                     </li>
                 </ul>
