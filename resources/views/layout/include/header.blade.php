6db11f25b98b11f5b600bf6d12ed4fde8c6699ac
Ramandhika <ramandhika.ilham@gmail.com>
2024-07-16 03:52:39 +0700

    Refactor dataRekening.blade.php to update select options for Kelompok Rekening and Tipe Rekening

--- resources/views/layout/include/header.blade.php
+++ resources/views/layout/include/header.blade.php
@@ -25,7 +25,7 @@
                                             class="absolute inline-flex items-center justify-center  text-white text-[11px] font-medium  bg-blue-600 w-2 h-2 rounded-full -top-[1px] -right-[6px]">
                                         </div>
                                     </a>
-                                    <div class="card hs-dropdown-menu transition-[opacity,margin] border border-gray-400 rounded-md duration hs-dropdown-open:opacity-100 opacity-0 mt-2 min-w-max  w-[300px] hidden z-[12]"
+                                    {{-- <div class="card hs-dropdown-menu transition-[opacity,margin] border border-gray-400 rounded-md duration hs-dropdown-open:opacity-100 opacity-0 mt-2 min-w-max  w-[300px] hidden z-[12]"
                                         aria-labelledby="hs-dropdown-custom-icon-trigger">
                                         <div>
                                             <h3 class="text-gray-600 font-semibold text-base px-6 py-3">Notification
@@ -73,7 +73,7 @@
                                                 </li>
                                             </ul>
                                         </div>
-                                    </div>
+                                    </div> --}}
                                 </div>
 
                             </li>
